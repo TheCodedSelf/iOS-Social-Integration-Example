934850b69ff5a3d67d248af83e1a5df2c79d9f1a
Keegan Rush <thecodedself@gmail.com>
2016-12-25 09:52:57 +0200

    Build example project

--- SocialIntegrationPOC/ViewController.swift
+++ SocialIntegrationPOC/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var connectToFacebookButton: UIButton!
+    @IBOutlet weak var connectToLinkedInButton: UIButton!
 
+    @IBOutlet weak var viewFacebookDataButton: UIButton!
+    @IBOutlet weak var viewLinkedInDataButton: UIButton!
+    
+    let facebookConnected = false
+    let linkedInConnected = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.title = "Social Integration POC"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        connectToFacebookButton.isHidden = facebookConnected
+        connectToLinkedInButton.isHidden = linkedInConnected
+        viewFacebookDataButton.isHidden = !facebookConnected
+        viewLinkedInDataButton.isHidden = !linkedInConnected
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func connectToFacebook(_ sender: Any) {
+    }
 
-
+    @IBAction func connectToLinkedIn(_ sender: Any) {
+    }
 }
 
