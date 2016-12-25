27e8da767df76415504adba5f9964eab8d5920ff
Keegan Rush <thecodedself@gmail.com>
2016-12-25 08:54:53 +0200

    Initial Commit

---
+++ SocialIntegrationPOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialIntegrationPOC
+//
+//  Created by Keegan Rush on 2016/12/25.
+//  Copyright © 2016 Shnapped. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
